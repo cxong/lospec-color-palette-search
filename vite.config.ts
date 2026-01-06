9efe65d692ba887cebc5be7f7fa171c7f8863a4f
congusbongus <congusbongus@gmail.com>
2026-01-07 02:47:37 +1100

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/lospec-color-palette-search/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
